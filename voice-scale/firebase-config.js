093038d52fa3d3a0da61586ce73ba278f626d571
AriaKawa <AriaKawa@users.noreply.github.com>
2026-05-13 10:41:19 -0500

    Configure VoiceScale Firebase project

--- voice-scale/firebase-config.js
+++ voice-scale/firebase-config.js
@@ -1,8 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDRniZatGeylxphjHQadYjucOcirNBRIdk",
+  authDomain: "multiplayer-640ec.firebaseapp.com",
+  databaseURL: "https://multiplayer-640ec-default-rtdb.firebaseio.com",
+  projectId: "multiplayer-640ec",
+  storageBucket: "multiplayer-640ec.firebasestorage.app",
+  messagingSenderId: "94914236381",
+  appId: "1:94914236381:web:55ab00cc690140180cf034",
+  measurementId: "G-V43J1S8RGF",
 };
